ad72e7cc93807213247834a3750e3ce67383b139
Donghee <hongricky73@naver.com>
2024-06-17 20:14:11 +0900

    logo.svg update

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,11 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
+class HomeScreen extends StatefulWidget {
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
 
-class HomeScreen extends StatelessWidget {
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Home Screen'),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          icon: SvgPicture.asset('assets/icons/logo.svg'),
+          onPressed: () {}, //refresh 새로고침 기능!
+
+        ),
+      ),
+
     );
   }
 }
+
+
