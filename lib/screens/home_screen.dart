8e3326b07f5f06d4fb66326e48848569b070da6c
Donghee <hongricky73@naver.com>
2024-06-18 18:01:58 +0900

    home_screen appbar update

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -13,11 +13,41 @@ class _HomeScreenState extends State<HomeScreen> {
       backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: Colors.white,
-        leading: IconButton(
-          icon: SvgPicture.asset('assets/icons/logo.svg'),
-          onPressed: () {}, //refresh 새로고침 기능!
-
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 8.0),
+          child: IconButton(
+            icon: SvgPicture.asset('assets/icons/logo.svg',
+            width: 28,height: 28,),
+            onPressed: () {},//refresh 새로고침 기능!
+          ),
         ),
+        title: Container(
+          height: 30,
+          child: TextField(
+            decoration: InputDecoration(
+              hintText: '검색어를 입력하세요.',
+              hintStyle: TextStyle(color: Colors.white,fontSize: 14),
+              fillColor: Colors.black,
+              filled: true,
+              prefixIcon: Icon(Icons.search,color: Colors.white,),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(20),
+                borderSide: BorderSide.none,
+              ),
+              contentPadding: EdgeInsets.zero,
+            ),
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 8.0),
+            child: IconButton(
+                onPressed: (){},
+                icon: SvgPicture.asset('assets/icons/bell.svg',
+                height: 28,width: 28,)),
+          )
+        ],
       ),
 
     );
