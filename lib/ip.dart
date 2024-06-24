5d2083e7194d7a282ba36a2aafc6280f2a33756a
Donghee <hongricky73@naver.com>
2024-06-24 19:02:07 +0900

    listbutton update

--- lib/ip.dart
+++ lib/ip.dart
@@ -1,2 +1,2 @@
-String address = "59.11.250.211:8080";
+String address = "220.88.78.16:8080";
 
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,16 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:bemajor_frontend/models/community.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'community/post_list_screen.dart';
+import '/api_url.dart';
+import '../../models/community.dart';
+
+Future<String?> readJwt() async {
+  final prefs = await SharedPreferences.getInstance();
+  return prefs.getString('USERID');
+}
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -9,17 +18,41 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  late List<BoardDto> boards = [];
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchBoards();
+  }
+
+  Future<void> fetchBoards() async {
+    String? token = await readJwt();
+    final response = await http.get(
+      Uri.parse('${ApiUrl.baseUrl}/api/board'),
+      headers: {'authorization': '$token'},
+    );
+    if (response.statusCode == 200) {
+      final List<dynamic> jsonData = jsonDecode(response.body);
+      setState(() {
+        boards = jsonData.map((data) => BoardDto.fromJson(data)).toList();
+        isLoading = false;
+      });
+    } else {
+      throw Exception('Failed to load boards');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: _HomeAppbar(),
-      body: _HomeBody(),
+      body: isLoading ? Center(child: CircularProgressIndicator()) : _HomeBody(boards: boards),
     );
   }
 }
-
-
 
 PreferredSizeWidget _HomeAppbar() {
   return AppBar(
@@ -69,33 +102,43 @@ PreferredSizeWidget _HomeAppbar() {
   );
 }
 
-
 class _HomeBody extends StatelessWidget {
+  final List<BoardDto> boards;
 
-  const _HomeBody({super.key});
+  const _HomeBody({Key? key, required this.boards}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         Container(
-
           height: 40, // 버튼 컨테이너의 높이 설정
           child: SingleChildScrollView(
             scrollDirection: Axis.horizontal,
             child: Row(
-              children: List.generate(10, (index) {
+              children: List.generate(boards.length, (index) {
                 return Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 8.0),
                   child: ElevatedButton(
                     onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => PostListScreen(
+                            boards[index].boardName,
+                            boards[index].id,
+                          ),
+                        ),
+                      );
                     },
                     style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.black,
-                        minimumSize: Size(114,28)
+                      backgroundColor: Colors.black,
+                      minimumSize: Size(114, 28),
                     ),
-                    child: Text('버튼 ${index + 1}',style: TextStyle(color: Colors.white),),
-
+                    child: Text(
+                      boards[index].boardName,
+                      style: TextStyle(color: Colors.white),
+                    ),
                   ),
                 );
               }),
@@ -109,7 +152,5 @@ class _HomeBody extends StatelessWidget {
     );
   }
 }
-
-
 
 
