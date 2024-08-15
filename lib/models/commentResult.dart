6929a1de8c0579f1a8b1fc7893d0b8839d3deed8
DESKTOP-ME5QO6B\admin <wlwlwlal2@gmail.com>
2024-08-15 17:07:44 +0900

    게시글에 댓글 안뜨는 오류 수정

--- lib/models/commentResult.dart
+++ lib/models/commentResult.dart
@@ -10,55 +10,81 @@ class CommentResult {
   final int status;
   final bool userCheck;
 
+  final String? userName;
+  final String? email;
+  final String? birth;
+  final String? oauth2Id;
+  final String? role;
+  final String? belong;
+  final String? department;
+
   final Map<String, dynamic>? reply;
-  final Map<String, dynamic>? user;
 
   CommentResult({
-  required this.id,
-  required this.content,
-  required this.goodCount,
-  required this.commentDate,
-  required this.postId,
-  required this.parentId,
-  required this.dateDiff,
-  required this.isFavorite,
-  required this.status,
-  required this.reply,
-  required this.user,
-  required this.userCheck
+    required this.id,
+    required this.content,
+    required this.goodCount,
+    required this.commentDate,
+    required this.postId,
+    required this.parentId,
+    required this.dateDiff,
+    required this.isFavorite,
+    required this.status,
+    required this.reply,
+    required this.userCheck,
+    required this.userName,
+    required this.email,
+    required this.birth,
+    required this.oauth2Id,
+    required this.role,
+    required this.belong,
+    required this.department
   });
 
   factory CommentResult.fromJson(Map<String, dynamic> json) {
-  return CommentResult(
-  id: json['id'],
-  content: json['content'],
-  goodCount: json['goodCount'],
-  commentDate: json['commentDate'],
-  postId: json['postId'],
-  parentId: json['parentId'],
-  dateDiff: json['dateDiff'],
-  isFavorite: json['favorite'],
-  status: json['status'],
-  reply: json['reply'],
-  user: json['user'],
-  userCheck: json['userCheck']
-  );
+    return CommentResult(
+      id: json['id'],
+      content: json['content'],
+      goodCount: json['goodCount'],
+      commentDate: json['commentDate'],
+      postId: json['postId'],
+      parentId: json['parentId'],
+      dateDiff: json['dateDiff'],
+      isFavorite: json['favorite'],
+      status: json['status'],
+      reply: json['reply'],
+      userCheck: json['userCheck'],
+      userName: json['userName'],
+      email: json['email'],
+      birth: json['birth'],
+      oauth2Id: json['oauth2Id'],
+      role: json['role'],
+      belong: json['belong'],
+      department: json['department'],
+
+    );
   }
 
   Map<String, dynamic> toJson() {
-  return {
-  'id': id,
-  'content': content,
-  'goodCount': goodCount,
-  'commentDate': commentDate,
-  'postId': postId,
-  'parentId': parentId,
-  'dateDiff': dateDiff,
-  'status': status,
-  'Favorite': isFavorite,
-  'reply': reply,
-  'user': user,
-   'userCehck': userCheck
-  };
+    return {
+      'id': id,
+      'content': content,
+      'goodCount': goodCount,
+      'commentDate': commentDate,
+      'postId': postId,
+      'parentId': parentId,
+      'dateDiff': dateDiff,
+      'status': status,
+      'Favorite': isFavorite,
+      'reply': reply,
+      'userCehck': userCheck,
+      'userName': userName,
+      'email': email,
+      'birth': birth,
+      'oauth2Id': oauth2Id,
+      'role': role,
+      'belong': belong,
+      'department': department,
+    };
   }
-  }
+}
